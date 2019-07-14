e3d27a0f701506c26c4d8ee3acfa2a6911cbe4de
unknown <theechofive@gmail.com>
2019-07-14 22:08:14 +0300

    Server

--- server/server.js
+++ server/server.js
@@ -4,33 +4,36 @@ var express = require("express");
 var request = require("request");
 var Core = /** @class */ (function () {
     function Core(port) {
+        this.exchanges = [
+            { "name": 'coindesk', "url": "https://api.coindesk.com/v1/bpi/currentprice/btc.json", "path": 'bpi.USD.rate', "rate": "" },
+        ];
         this.port = port;
     }
-    Core.prototype.sendRequest = function (uri, callback) {
-        return request.get(uri, callback);
+    Core.prototype.getRates = function () {
+        var _loop_1 = function (exchange) {
+            request.get(exchange.url, function (error, response, body) {
+                body = JSON.parse(body);
+                console.log(body.bpi.USD.rate);
+                exchange.rate = this['body.' + exchange.path];
+                // exchange.rate = exchange.rate.replace(',', '')
+                console.log(exchange.rate);
+            });
+        };
+        for (var _i = 0, _a = this.exchanges; _i < _a.length; _i++) {
+            var exchange = _a[_i];
+            _loop_1(exchange);
+        }
     };
     Core.prototype.onStart = function () {
+        var _this = this;
         var app = express();
         app.listen(this.port, function () {
-            console.log('server started');
+            console.log("Server started | " + _this.port);
         });
     };
     ;
     return Core;
 }());
 var core = new Core(3000);
-core.sendRequest('https://api.coindesk.com/v1/bpi/currentprice/btc.json', function (error, response, body) {
-    var coindesk = JSON.parse(body);
-    console.log(coindesk.bpi.USD.rate);
-});
+core.getRates();
 core.onStart();
-// https://api.coindesk.com/v1/bpi/currentprice/btc.json
-//  exchanges.coindesk.bpi.USD.rate
-// https://blockchain.info/ticker
-//exchanges.blockchain.USD.last
-//https://api.coinbase.com/v2/prices/spot?currency=USD
-//exchanges.coinbase.data.amount);
-// https://www.bitstamp.net/api/v2/ticker/btcusd/
-// exchanges.bitstamp.last
-// https://bitpay.com/api/rates
-// exchanges.bitpay[2].rate
--- server/server.ts
+++ server/server.ts
@@ -1,49 +1,42 @@
 import * as express from 'express';
 import * as request from 'request';
 import {App} from '../types';
-import { json } from 'body-parser';
 
 class Core implements App {
 
     public port: number;
+    public exchanges: { name:string, url:string, path:string, rate: string }[] = [
+        { "name": 'coindesk', "url": "https://api.coindesk.com/v1/bpi/currentprice/btc.json", "path": 'bpi.USD.rate', "rate": "" },
+        // { "name": 'blockchain', "url": "https://blockchain.info/ticker", "path": 'USD.last', "rate": "" },
+        // { "name": 'coinbase', "url": "https://api.coinbase.com/v2/prices/spot?currency=USD", "path": 'data.amount', "rate": "" },
+        // { "name": 'bitstamp', "url": "https://www.bitstamp.net/api/v2/ticker/btcusd/", "path": 'last', "rate": "" },
+        // { "name": 'bitpay', "url": "https://bitpay.com/api/rates", "path": 'bitpay[2].rate', "rate": "" }
+    ];
 
     constructor(port:number) {
         this.port = port;
     }
 
-    sendRequest(uri: string, callback?: (error: any, response: request.Response, body: any) => void): request.Request {
-       return request.get(uri, callback)
+    getRates() {
+       for (const exchange of this.exchanges) {
+        request.get(exchange.url,function(error,response,body){
+            body = JSON.parse(body);
+            console.log(body);
+            //exchange.rate = this['body.'+ exchange.path]; 
+            //exchange.rate = exchange.rate.replace(',', '')
+            console.log(exchange.rate); 
+        })
+    }
     }
 
     onStart(){
         const app = express();
         app.listen(this.port, () => {
-            console.log('server started'); 
+            console.log(`Server started | ${this.port}`); 
         });
     };
 
 }
 let core = new Core(3000);
-core.sendRequest('https://api.coindesk.com/v1/bpi/currentprice/btc.json', function(error, response, body){
-    let coindesk = JSON.parse(body)
-    console.log(coindesk.bpi.USD.rate);
-   })
-
-   
+core.getRates();
 core.onStart();
-
-//exchanges.coindesk = exchanges.coindesk.replace(',', '')
-// https://api.coindesk.com/v1/bpi/currentprice/btc.json
-//  exchanges.coindesk.bpi.USD.rate
-
-// https://blockchain.info/ticker
-//exchanges.blockchain.USD.last
-
-//https://api.coinbase.com/v2/prices/spot?currency=USD
-//exchanges.coinbase.data.amount);
-
-// https://www.bitstamp.net/api/v2/ticker/btcusd/
-// exchanges.bitstamp.last
-
-// https://bitpay.com/api/rates
-// exchanges.bitpay[2].rate
